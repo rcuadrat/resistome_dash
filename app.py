de15f4c3787347c4501606b66b4ee1dda26bbab5
Rafael Cuadrat <rafaelcuadrat@gmail.com>
2019-11-20 12:56:07 +0100

    fixing bugs

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import plotly_express as px
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 
 from dash.dependencies import Input, Output
 import pandas as pd

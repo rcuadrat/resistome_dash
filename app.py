84f247ede6a1b527b4a3497a503d25c2627fbb2a
Rafael Cuadrat <rafaelcuadrat@gmail.com>
2019-11-28 17:42:57 +0100

    adding explore by class

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 ###
 app = dash.Dash(__name__)
+server = app.server
 
 app.layout = html.Div([html.Div(className="pretty_container",
                                 children=[
